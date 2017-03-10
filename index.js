f8b4d11452dd7687edae7b9791b3bf01761392dd
seanzihunterelli <sean.s.hoffman@gmail.com>
2017-03-09 19:44:34 -0700

    finished basic project

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+app.listen(8181);
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+const messages = [{message:"Not right now"}, {message:"Maybe later"}];
+
+app.get('/messages', (req, res) => {
+    res.status(200).json({ messages: messages});
+});
+
+app.post('/messages', (req, res, next) => {
+  messages.push({message:req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+  console.log(req.body);
+});
